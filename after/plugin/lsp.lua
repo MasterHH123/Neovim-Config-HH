0da5e9186d8c5b9a6393229e2027e27a17c96a1f
Horacio H <hernandezh4@hotmail.com>
2024-03-22 21:23:40 -0700

    Added language-server support for C, Java, Python, Rust and JavaScript/TypeScript

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,35 @@
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+require'lspconfig'.clangd.setup{}
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.rust_analyzer.setup{}
+require'lspconfig'.tsserver.setup{}
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),	
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),	
+	['<C-y>'] = cmp.mapping.confirm({ select = true}),	
+	['<C-space>'] = cmp.mapping.complete()	
+})
+
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp.default_setup,
+  },
+})
+
+lsp.setup()
